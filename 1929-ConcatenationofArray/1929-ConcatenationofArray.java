d34228087efe8b5e3d1ed6242f3b112606e09414
Shaheer <64867718+ShaheerVD@users.noreply.github.com>
2024-01-16 15:09:47 +0200

    Create file

---
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.java
@@ -0,0 +1,14 @@
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+        } 
+        int index = nums.length;
+        for(int i =0;i<nums.length;i++){
+            ans[index] = nums[i];
+        }
+    }
+            index++;
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[2*nums.length];
+        return ans;
+[
